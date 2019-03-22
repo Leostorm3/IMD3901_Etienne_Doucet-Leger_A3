1912dd5d213e57fd13b0026037f5da4ea7a6e4df
Etienne Doucetleger <douc0131@algonquinlive.com>
2019-03-22 17:12:00 -0400

    server workd

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const gameState = {
     players: {}
   }
 
+  
+
 app.use((express.static(__dirname + '/public')));
 
 app.get('/PlayerDesktop', function(req,res) {
@@ -24,31 +26,51 @@ app.get('/Phone', function(req,res) {
 
 socketIO.on('connection',function(socket) {
     console.log( socket.id + ' has connected to port' );
+    
 
     socket.on('PlayerJoining', function(data){
         gameState.players[socket.id] = {
           x: 0,
-          y: 0,
-          z:0,
+          y: 1.6,
+          z: -5,
           width: 25,
           height: 25,
-          energy:100
+          energy:100,
+          name:socket.id
           
         };
       });
 
 
+      socket.on('move', function(data) {
+        var player = gameState.players[socket.id] || {};
+        if (data.left) {
+          player.x -= 0.1;
+        }
+        if (data.up) {
+          player.z -= 0.1;
+        }
+        if (data.right) {
+          player.x += 0.1;
+        }
+        if (data.down) {
+          player.z += 0.1;
+        }
+      });
+    
+
     socket.on('disconnect', function(data){
+        socketIO.sockets.emit('delete_user', gameState.players[socket.id].name);
         delete gameState.players[socket.id];
         console.log( socket.id + ' has disconnect to port' );
     });
 
-    
-    //we send to all server the updates of the game to make sure all players are created in the world
-    setInterval(() => {
-    socket.emit('state', gameState);
-  }, 1000 / 60);
+        
 });
+//we send to all server the updates of the game to make sure all players are created in the world
+setInterval(function(){
+    socketIO.sockets.emit('state', gameState);
+  }, 1000 / 60);
 
 
 
--- public/js/Main_game.js
+++ public/js/Main_game.js
@@ -1,10 +1,142 @@
 let socket = io();
 
- 
-socket.emit('PlayerJoining');
+var sessionid;
+
+
+const scene = document.querySelector('a-scene');
 //here is a premade function to generate a player on the terrain
 const GeneratePlayer = function(player) {
 
-    let CowElem = document.createElement('a-entity');//create new element
+    
+        //here we check if its the player the joined in if yes we are makibg a cam to follow
+        let PCam = document.createElement('a-entity');//create new element
+            PCam.setAttribute('camera');
+            PCam.setAttribute('position',player.x + " " + player.y + " " + player.z);
+            PCam.setAttribute('id',player.name);
+            
+        let Char = document.createElement('a-entity');//create new element
+            Char.setAttribute('geometry',  "primitive: box" );
+            Char.setAttribute('position',0 + " " + 0 + " " + 5);
+            Char.setAttribute('scale',1 + " " + 1 + " " + 1);
+            Char.setAttribute('material',"color:"+ colorgen());
+            PCam.appendChild(Char);
+            scene.appendChild(PCam);
+            if(sessionid == player.name){
+                var truecam = document.querySelector(player.name);
+                truecam.setAttribute('camera', 'active', true);
+            }
+
+
+             
+        
+       
+                
 
 };
+
+const MoveUser = function(player){
+    let User = document.getElementById(player.name);
+    User.setAttribute('position',player.x + " " + player.y + " " + player.z);
+
+}
+const deleteUser = function(id){
+    let User = document.getElementById(id);
+    User.parentNode.removeChild(User);
+
+}
+
+
+var PlayerMove = {
+    up: false,
+    down: false,
+    left: false,
+    right: false
+  }
+
+  function generationNumber255() {
+    return (Math.floor(Math.random() * 255));
+}
+
+  function colorgen(){
+    return (generationNumber255() + " " + generationNumber255() + " " + generationNumber255());
+
+
+  }
+
+  function ObjectLength( object ) {
+    var length = 0;
+    for( var key in object ) {
+        if( object.hasOwnProperty(key) ) {
+            ++length;
+        }
+    }
+    return length;
+};
+
+
+  document.addEventListener('keydown', function(event) {
+    switch (event.keyCode) {
+      case 65: // A
+      PlayerMove.left = true;
+        break;
+      case 87: // W
+      PlayerMove.up = true;
+        break;
+      case 68: // D
+      PlayerMove.right = true;
+        break;
+      case 83: // S
+      PlayerMove.down = true;
+        break;
+    }
+  });
+  document.addEventListener('keyup', function(event) {
+    switch (event.keyCode) {
+      case 65: // A
+      PlayerMove.left = false;
+        break;
+      case 87: // W
+      PlayerMove.up = false;
+        break;
+      case 68: // D
+      PlayerMove.right = false;
+        break;
+      case 83: // S
+      PlayerMove.down = false;
+        break;
+    }
+  });
+
+socket.emit('PlayerJoining');
+setInterval(function(){
+    socket.emit('move', PlayerMove);
+  }, 1000 / 60);
+
+socket.on('state', function(gameState){
+    //console.log(gameState.players);
+   
+        for (var id in gameState.players) {
+        var element = document.getElementById(gameState.players[id].name);
+        if(element == null ){
+        var player = gameState.players[id];
+        GeneratePlayer(player);
+        }
+    }
+    
+        for (var id in gameState.players) {
+            var player = gameState.players[id];
+            MoveUser(player);
+    
+    }
+  });
+
+
+  socket.on('delete_user', function(data){
+    deleteUser(data);
+  });
+
+  
+socket.on('connect', function() {
+   sessionid = socket.id; 
+   //console.log(sessionid);
+});
